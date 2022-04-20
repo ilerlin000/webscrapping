0c940a411be156c459008d111bae0eadc526a51d
Lindsey Iler <lindsey_iler1@baylor.edu>
2022-04-19 22:01:54 -0500

    ze homework

--- webscraping-Bible.py
+++ webscraping-Bible.py
@@ -17,7 +17,7 @@ url = 'https://ebible.org/asv/JHN' + random_chapter + '.htm'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 req = Request(url, headers=headers)
 
-webpage = urlopen(req).read
+webpage = urlopen(req).read()
 
 soup = BeautifulSoup(webpage, 'html.parser')
 
